b1844dfaf6c7704de88b5f14bb2cc68363e9419c
EvgeniyMVas <Evgeniyvas1984@gmail.com>
2024-10-12 15:59:15 +0300

    Add project files.

---
+++ ComponentsPractic/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ComponentsPractic.Models
+{
+	public class User
+	{
+		public string Name { get; set; }
+		public int Score {  get; set; }
+	}
+}
---
+++ ComponentsPractic/ViewComponents/UserViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using ComponentsPractic.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComponentsPractic.ViewComponents
+{
+	public class UserViewComponent: ViewComponent
+	{
+		public IViewComponentResult Invoke(User user)
+		{
+
+
+			
+			var users = new List<User>
+			{
+				new User { Name = "Tom", Score = 156 },
+				new User { Name = "Bob", Score = 315 },
+				new User { Name = "Anna", Score = 250 },
+				new User { Name = "Dave", Score = 98 },
+				new User { Name = "Eva", Score = 164 },
+			
+			};
+			var topGamers = users.OrderByDescending(u => u.Score).Take(3).ToList();
+
+			return View(topGamers);
+		}	
+	}
+}
